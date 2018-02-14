b3147f49cacf721d0a85f2e19387bdd54f866df3
Andrew Danger Lyon <orthecreedence@gmail.com>
2018-02-13 21:43:52 -0800

    initial commit, allows loading a yaml file (sync) and blanketing ENV vars over it. works nicely, returns the entire parsed object.

---
+++ main.js
@@ -0,0 +1,42 @@
+var fs = require('fs');
+var yaml = require('js-yaml');
+
+exports.load = function(prefix, yaml_file) {
+	if(!yaml_file) throw new Error('invalid yaml file given');
+	var contents = fs.readFileSync(yaml_file);
+	var config_obj = yaml.safeLoad(contents);
+
+	function check_env(path, default_val) {
+		var key = (prefix+'_'+path.join('_')).toUpperCase();
+		var val = process.env[key];
+		if(typeof(val) == 'undefined') {
+			val = default_val;
+		}
+		return val;
+	}
+
+	function walk_obj(obj, path) {
+		if(Array.isArray(obj)) {
+			var whole_array = check_env(path, null);
+			if(whole_array) {
+				obj = JSON.parse(whole_array);
+			} else {
+				for(var i = 0; i < obj.length; i++) {
+					obj[i] = walk_obj(obj[i], path.concat([i]));
+				}
+			}
+		} else if(obj !== null && typeof(obj) == 'object') {
+			Object.keys(obj).forEach(function(key) {
+				obj[key] = walk_obj(obj[key], path.concat([key]));
+			});
+		} else {
+			obj = check_env(path, obj);
+		}
+		return obj;
+	}
+	config_obj = walk_obj(config_obj, []);
+	console.log('---\n'+JSON.stringify(config_obj, null, 2));
+};
+
+exports.load('TURTL', 'c:/htdocs/turtl/server/config/config.yaml');
+
